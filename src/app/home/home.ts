0ce00bc76e3a33f6385b3d663d0fc8f9b33c0c45
Bartlomiej Gotfryd <barto.gotfryd33@gmail.com>
2025-09-23 20:34:47 +0200

    Search functionallity added based on users city.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -13,7 +13,22 @@ export class Home {
   housingLocationList: HousingLocationInfo[] = [];
 
   housingService: Housing = inject(Housing);
+
+  filteredLocationList: HousingLocationInfo[] = [];
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
